d315f75377ee418e5fabf927980e3729e6372fe1
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-08 17:11:44 -0500

    created homepage component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  return (
-    <>
-      <h1>Music Therapy Services In Austin</h1>
-    </>
-  );
-}
+export const App = () => {
+  return <></>;
+};
 
 export default App;
