0314062f68a5492a2af2cd7882226c4477ed5343
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-15 16:09:20 -0500

    contact form set up with email.js

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,14 +1,41 @@
 import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
+  const serviceId = "service_fgob5cf";
+  const templateId = "template_s8bncad";
+  const publicKey = "8PMKhMX1-PNCW9h4V";
+
+  const templateParams = {
+    from_name: name,
+    from_email: email,
+    message: message,
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log("email sent!", response);
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.error("Something went wrong", error);
+      });
+  };
+
   return (
     <div className="contactPage">
       <h1>Contact</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Name: </label>
         <input
           type="text"
