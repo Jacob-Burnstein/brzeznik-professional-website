369b68e3d5833a5421cac33a8d9f551df26849f3
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-15 15:57:16 -0500

    useState tracking onChange values on contact form

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <div className="contactPage">
       <h1>Contact</h1>
       <form>
         <label>Name: </label>
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
         <label>Email: </label>
-        <input type="text" />
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label>Message: </label>
-        <input className="messageInput" type="text" />
+        <input
+          className="messageInput"
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
