121e11bfe87cf84518cbf43a18f8f1729a552ac2
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-19 16:50:31 -0500

    replaced checkmarks with icons

--- src/components/home-subcomponents/ServicesOffered.jsx
+++ src/components/home-subcomponents/ServicesOffered.jsx
@@ -2,22 +2,19 @@ import React from "react";
 
 const ServicesOffered = () => {
   const services = [
-    "Initial Assessment",
-    "Individual Tx",
-    "Group Tx",
-    "etc",
-    "etc",
+    { name: "Initial Assessment", icon: "fa-solid fa-clipboard-list" },
+    { name: "Individual Tx", icon: "fa-solid fa-person-half-dress" },
+    { name: "Group Tx", icon: "fa-solid fa-people-group" },
   ];
-
   return (
     <>
       <section className="servicesList">
         <h2>Services Offered</h2>
         <ul>
           {services.map((service, index) => (
-            <div className="serviceAndCheck" key={index}>
-              <i className="fa-solid fa-check fa-2x checkmark"></i>
-              <li className="service">{service}</li>
+            <div className="serviceAndIcon" key={index}>
+              <i className={service.icon}></i>
+              <li className="service">{service.name}</li>
             </div>
           ))}
         </ul>
