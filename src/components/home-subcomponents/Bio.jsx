d9adc19b24bd07f4ccec420cf3cc763b1a4b5528
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-25 10:39:29 -0500

    updated bio text

--- src/components/home-subcomponents/Bio.jsx
+++ src/components/home-subcomponents/Bio.jsx
@@ -5,28 +5,32 @@ const Bio = () => {
   return (
     <section className="bio">
       <p className="intro">
-        Brianna Rzeznik (she/her) is a Masters-level music therapist and
-        psychotherapist currently practicing in Austin, Texas.
-      </p>
-
-      <p>
-        Brianna holds her Masters in Clinical Mental Health Counseling with a
-        music therapy specialization from Lesley University. She has worked as a
-        music therapist in hospitals, schools, and homes; her clients include
-        adults and children with Autism and other intellectual and developmental
-        disabilities, older adults (including those with Alzheimer's and
-        Dementia), individuals with acute psychiatric symptoms, individuals
-        dealing with or recovering from medical diagnoses or procedures, and
-        children experiencing such things as homelessness, parental
-        incarceration, and parental deportation. Brianna also works as a
-        psychotherapist at the Center for Therapy and Healing in Boston, MA,
-        where she provides individual therapy sessions for clients via
-        telehealth.
+        Brianna Rzeznik is a Masters-level music therapist, currently practicing
+        in Austin, Texas.
       </p>
       <p>
-        Brianna believes that mental and emotional wellbeing is a priority for
-        every client, regardless of who they are or why they are in music
-        therapy. Her approach is warm, accepting, and authentic.
+        With extensive experience spanning hospitals, schools, and homes,
+        Brianna has worked with a diverse clientele, including adults and
+        children with Autism and other developmental disabilities, older adults
+        with Alzheimer’s and Dementia, individuals facing acute psychiatric
+        symptoms, and those navigating medical diagnoses or procedures. She also
+        runs music therapy groups to support children dealing with challenges
+        such as homelessness, parental incarceration, grief and bereavement, and
+        parental deportation in schools in and around Austin.
+      </p>
+      <p>
+        Brianna's core belief is that mental and emotional well-being are
+        fundamental for every individual, regardless of their circumstances. Her
+        approach to therapy is characterized by warmth, acceptance, and
+        authenticity, creating a nurturing environment for creativity and skill
+        development.
+      </p>
+      <p>
+        In addition to her work as a music therapist, Brianna is also a
+        practicing psychotherapist at the Center for Therapy and Healing, a
+        telehealth practice based in Boston, Massachusetts. There, she provides
+        compassionate guidance to adolescents and adults, empowering them on
+        their journey towards enhanced well-being and personal growth.
       </p>
     </section>
   );
