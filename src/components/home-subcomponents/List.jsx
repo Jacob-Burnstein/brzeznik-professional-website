f0cf76c60455c2ee2debeb95bcaf7bf6166650fa
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-02-25 11:31:46 -0500

    added descriptions to List.jsx

--- src/components/home-subcomponents/List.jsx
+++ src/components/home-subcomponents/List.jsx
@@ -5,19 +5,23 @@ const List = () => {
   const namesAndDescriptions = [
     {
       name: "Physical Disabilites",
-      description: "Here's how",
+      description:
+        "Utilizing music therapy techniques, individuals with physical disabilities can benefit from improved gross and fine motor skills, enhanced coordination, increased grip strength, and better ability to cross the midline. By incorporating instrument play and movement to music, sessions can facilitate gait training and elevate overall physical capabilities. Moreover, integrating physical tasks and exercises with music transforms routine activities into enjoyable experiences, fostering engagement and motivation.",
     },
     {
       name: "Intellectual/Developmental Disabilities",
-      description: "Here's how",
+      description:
+        "Music therapy is a valuable tool for individuals with intellectual and developmental disabilities, offering opportunities for expressive language development, sustained attention, cognitive growth, and enhanced social engagement. The structured nature of musical activities aids in the memorization of task sequences, important rules, and acquisition of new skills. Within group settings, music provides a safe and structured platform for socialization, fostering meaningful connections and interactions among peers.",
     },
     {
       name: "Mental Health",
-      description: "Here's how",
+      description:
+        "Music therapy is a versatile approach to promoting mental health and well-being across various care settings, including inpatient and outpatient environments. Music serves as a powerful medium for individuals to express their experiences, emotions, and thoughts, both verbally and non-verbally. It fosters healthy relationships, facilitates communication, and aids in reconnecting individuals with their bodies. Moreover, music therapy can play a vital role in gender-affirming care and mitigating feelings of isolation, anxiety, depression, and fear. Integrating music therapy with psychotherapeutic techniques enhances treatment outcomes; for instance, within dialectical behavior therapy (DBT), music interventions effectively support distress tolerance and emotional regulation skills development.",
     },
     {
       name: "Memory Care",
-      description: "Here's how",
+      description:
+        "Music therapy is a valuable approach for individuals with dementia, Alzheimer's, and other neurodegenerative conditions, aimed at supporting memory care and overall wellbeing. Through music, individuals can retain expressive language abilities and experience sensory enrichment. Utilizing familiar songs aids in orientation to person, place, and time, while incorporating musical instruments encourages movement and facilitates neurorehabilitation. The structured nature of music provides a calming influence, effectively reducing stress, anxiety, depression, and agitation commonly associated with these conditions.",
     },
   ];
 
