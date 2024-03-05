2290641cedde745406511032a59386dcd085aa1c
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-03-05 17:03:20 -0500

    removed questions card

--- src/components/home-subcomponents/Questions.jsx
+++ src/components/home-subcomponents/Questions.jsx
@@ -1,25 +1,30 @@
 import React from "react";
-import QuestionsCard from "./QuestionsCard";
 
 const Questions = () => {
-  const questionsAndAnswers = [
-    {
-      question: "What Is Music Therapy?",
-      answer:
-        "Music therapy is the evidence-based use of music activities to help individuals and groups build skills or achieve personal goals. Music therapy is an allied health profession, just like physical therapy, occupational therapy, and speech therapy. Music therapy clientele range from infants to older adults with varying needs and disabilities.",
-    },
-    {
-      question: "Who are Music Therapists?",
-      answer:
-        "Music therapists are more than just music teachers or entertainers: music therapists are trained professionals who specialize in selecting and using music interventions - or activities that involve music - to target a client's unique goals. To be a music therapist, one must hold a Bachelor's degree or higher from a college or university program that's been approved by the American Music Therapy Association. In addition to educational coursework, they must compete at least 1200 supervised hours of clinical training, including a supervised internship. Lastly, they must pass the national board certification exam to obtain the credential MT-BC (Music Therapist-Board Certified), which is necessary for professional practice.",
-    },
-  ];
-
   return (
     <div className="questionSection">
-      {questionsAndAnswers.map(({ question, answer }, index) => (
-        <QuestionsCard key={index} question={question} answer={answer} />
-      ))}
+      <h2 className="question">What Is Music Therapy?</h2>
+      <p className="answer">
+        Music therapy is the evidence-based use of music activities to help
+        individuals and groups build skills or achieve personal goals. Music
+        therapy is an allied health profession, just like physical therapy,
+        occupational therapy, and speech therapy. Music therapy clientele range
+        from infants to older adults with varying needs and disabilities.
+      </p>
+      <h2 className="question">Who are Music Therapists?</h2>
+      <p className="answer">
+        Music therapists are more than just music teachers or entertainers:
+        music therapists are trained professionals who specialize in selecting
+        and using music interventions - or activities that involve music - to
+        target a client's unique goals. To be a music therapist, one must hold a
+        Bachelor's degree or higher from a college or university program that's
+        been approved by the American Music Therapy Association. In addition to
+        educational coursework, they must compete at least 1200 supervised hours
+        of clinical training, including a supervised internship. Lastly, they
+        must pass the national board certification exam to obtain the credential
+        MT-BC (Music Therapist-Board Certified), which is necessary for
+        professional practice.
+      </p>
     </div>
   );
 };
