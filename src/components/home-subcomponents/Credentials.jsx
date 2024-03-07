4b3f426891788f8dfcd27c2600f9c0fc16c96a46
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-03-06 19:52:36 -0500

    placeholder for credentials

---
+++ src/components/home-subcomponents/Credentials.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Credentials = () => {
+  const credentials = [
+    {
+      title: "Undergrad",
+      description:
+        "I hear the clock, it's six AM, I feel so far from where I've been",
+    },
+    {
+      title: "The Masters",
+      description:
+        "I got my eggs, I got my pancakes too. I got my maple syrup, everything but you",
+    },
+    {
+      title: "MT-BC",
+      description:
+        "I break the yolks and make a smiley face. I kinda like it in my brand new place",
+    },
+    {
+      title: "THISIS MY THESIS",
+      description:
+        "Wipe the spots up off the mirror. Don't leave my keys in the door. I never put wet towels on the floor anymore.",
+    },
+  ];
+  return <div></div>;
+};
+
+export default Credentials;
