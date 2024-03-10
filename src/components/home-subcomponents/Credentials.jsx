66f1c5a0ee9792b7cf0fd736129070a377717a6c
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-03-10 14:08:19 -0400

    tweaked format and added icon to credentials

--- src/components/home-subcomponents/Credentials.jsx
+++ src/components/home-subcomponents/Credentials.jsx
@@ -3,23 +3,24 @@ import React from "react";
 const Credentials = () => {
   const credentials = [
     {
-      title: "University of Massachusetts Amherst",
-      description: "BS in Communication Disorders",
-      description2: "BMus In Music",
+      title: "UNIVERSITY OF MASSACHUSETTS",
+      description: "BS IN COMMUNICATION DISORDERS",
+      description2: "BMus IN MUSIC",
     },
     {
-      title: "Lesley University",
-      description: "MA in Clinical Mental Health Counseling: Music Therapy",
+      title:
+        "LESLEY UNIVERSITY: MA IN CLINICAL MENTAL HEALTH COUNSELING: MUSIC THERAPY",
       description2:
         "Training in Lesley's graduate program prepares clinicians to address the emotional, cognitive, physical, and spiritual health of individuals using music as a lens for observation, research, and intervention.",
     },
     {
-      title: "Board-Certified Music Therapist (MT-BC)",
+      title: "BOARD-CERTIFIED MUSIC THERAPIST (MT-BC)",
       description:
         "I am a board-certified music therapist. This certification is issued by the Certification Board for Music Therapists (CBMT), which grants this credential to individuals who meet the requirements developed for this profession. This certification is recognized both nationally and internationally, and allows me to administer music therapy treatment independently and ethically.",
+      description2: "",
     },
     {
-      title: "Publication and Additional Specialties",
+      title: "PUBLICATION AND ADDITIONAL SPECIALITES",
       description:
         "Rzeznik, Brianna, 'The Use of Violin Family Instruments in Music Therapy with Individuals Experiencing Acute Psychiatric Presentations: A Literature Review' (2022). Expressive Therapies Capstone Theses. 556.",
       description2: "Read Here",
@@ -29,9 +30,10 @@ const Credentials = () => {
     <div className="credentialsContainer">
       {credentials.map((cred, index) => (
         <article key={cred.index} className="credentialCard">
+          <i class="fa-solid fa-toilet-paper fa-2x"></i>
           <h3>{cred.title}</h3>
           <p>{cred.description}</p>
-          {cred.description2 && cred.description2 === "Read Here" ? (
+          {cred.description2 === "Read Here" ? (
             <a href="https://digitalcommons.lesley.edu/expressive_theses/556/">
               <p>{cred.description2}</p>
             </a>
