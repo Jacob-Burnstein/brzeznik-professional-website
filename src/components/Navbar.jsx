1167a30979c7deaf456fe490566168fafab3e691
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-04-05 12:36:04 -0400

    functioning hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../index.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <nav className="navbar">
-      <ul className="navLinks">
-        <li onClick={() => navigate("/home")}>Home</li>
-        <p>|</p>
-        <li onClick={() => navigate("/music")}>Lessons & Performance</li>
-        <p>|</p>
-        <li onClick={() => navigate("/contact")}>Contact</li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navbar">
+        <ul className="navLinks">
+          <li onClick={() => navigate("/home")}>Home</li>
+          <p>|</p>
+          <li onClick={() => navigate("/music")}>Lessons & Performance</li>
+          <p>|</p>
+          <li onClick={() => navigate("/contact")}>Contact</li>
+        </ul>
+        <i class="fa-solid fa-bars fa-2x" onClick={() => handleClick()}></i>
+        <ul className={clicked ? "mobileNavLinks" : "hideMobileNavLinks"}>
+          <li
+            onClick={() => {
+              navigate("/home");
+              handleClick();
+            }}
+          >
+            Home
+          </li>
+          <li
+            onClick={() => {
+              navigate("/music");
+              handleClick();
+            }}
+          >
+            Lessons & Performance
+          </li>
+          <li
+            onClick={() => {
+              navigate("/contact");
+              handleClick();
+            }}
+          >
+            Contact
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 
