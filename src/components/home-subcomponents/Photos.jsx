21599151cd6258e218c25e37c6db1954e7134b95
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-04-12 16:33:20 -0400

    implemented carousel logic

--- src/components/home-subcomponents/Photos.jsx
+++ src/components/home-subcomponents/Photos.jsx
@@ -5,25 +5,39 @@ import dog from "../../images/dog.jpg";
 import violaSession from "../../images/viola-session.jpg";
 
 const Photos = () => {
+  const [selectedImage, setSelectedImage] = useState(0);
   const photos = [
-    { name: medical, className: "portrait", alt: "Brianna in PPE" },
-    { name: laughs, className: "landscape", alt: "Music therapy session" },
-    { name: dog, className: "portrait", alt: "Brianna and dog" },
     {
       name: violaSession,
       className: "landscape",
       alt: "Music therapy session",
     },
+    { name: medical, className: "portrait", alt: "Brianna in PPE" },
+    { name: laughs, className: "landscape", alt: "Music therapy session" },
+    { name: dog, className: "portrait", alt: "Brianna and dog" },
   ];
+
+  const goBack = () => {
+    setSelectedImage(selectedImage > 0 ? selectedImage - 1 : photos.length - 1);
+  };
+
+  const goNext = () => {
+    setSelectedImage(
+      selectedImage === photos.length - 1 ? 0 : selectedImage + 1
+    );
+  };
+
   return (
     <div className="home-photos">
-      {photos.map((photo, index) => (
-        <div>
-          <button> &lt;</button>
-          <img key={photo.index} src={photo.name} className={photo.className} />
-          <button> &lt;</button>
-        </div>
-      ))}
+      <div>
+        <button onClick={goBack}>&lt;</button>
+        <img
+          src={photos[selectedImage].name}
+          className={photos[selectedImage].className}
+          alt={photos[selectedImage].alt}
+        />
+        <button onClick={goNext}>&gt;</button>
+      </div>
     </div>
   );
 };
