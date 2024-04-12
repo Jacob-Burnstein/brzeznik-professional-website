eea8665a2c7569c566c7a71507771a2fa060b180
Jacob-Burnstein <129620016+Jacob-Burnstein@users.noreply.github.com>
2024-04-12 16:21:35 -0400

    imported new photo

--- src/components/home-subcomponents/Photos.jsx
+++ src/components/home-subcomponents/Photos.jsx
@@ -2,17 +2,27 @@ import React, { useState } from "react";
 import laughs from "../../images/laughs.jpeg";
 import medical from "../../images/medical.jpeg";
 import dog from "../../images/dog.jpg";
+import violaSession from "../../images/viola-session.jpg";
 
 const Photos = () => {
   const photos = [
     { name: medical, className: "portrait", alt: "Brianna in PPE" },
     { name: laughs, className: "landscape", alt: "Music therapy session" },
     { name: dog, className: "portrait", alt: "Brianna and dog" },
+    {
+      name: violaSession,
+      className: "landscape",
+      alt: "Music therapy session",
+    },
   ];
   return (
     <div className="home-photos">
       {photos.map((photo, index) => (
-        <img key={photo.index} src={photo.name} />
+        <div>
+          <button> &lt;</button>
+          <img key={photo.index} src={photo.name} className={photo.className} />
+          <button> &lt;</button>
+        </div>
       ))}
     </div>
   );
